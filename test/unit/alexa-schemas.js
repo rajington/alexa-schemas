afc24f0fd6edb105b6be2dc4d4b7d9737944d029
Rajarshi Nigam <raj@nigam.com>
2016-08-15 01:02:37 -0400

    test cases for JSON schemas

--- test/unit/alexa-schemas.js
+++ test/unit/alexa-schemas.js
@@ -1,18 +1,119 @@
-import alexaSchemas from '../../src/alexa-schemas';
+import fs from 'fs';
+import path from 'path';
+import chai from 'chai';
+import chaiJsonSchema from 'chai-json-schema';
+import yaml from 'js-yaml';
 
-describe('alexaSchemas', () => {
-  describe('Greet function', () => {
-    beforeEach(() => {
-      spy(alexaSchemas, 'greet');
-      alexaSchemas.greet();
+chai.use(chaiJsonSchema);
+chai.tv4.banUnknown = true;
+
+const loadSample = (...paths) =>
+  JSON.parse(fs.readFileSync(path.join(__dirname, 'samples', ...paths)));
+
+describe('request', () => {
+  let schema;
+
+  before(() => {
+    schema = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'skills-kit',
+      'request.yaml')));
+  });
+
+  describe('launch', () => {
+    it('should validate', () => {
+      chai.expect(loadSample('request', 'launch', 'base.json'))
+        .to.be.jsonSchema(schema);
     });
 
-    it('should have been run once', () => {
-      expect(alexaSchemas.greet).to.have.been.calledOnce;
+    it('should not validate with intent', () => {
+      chai.expect(loadSample('request', 'launch', 'withIntent.json'))
+        .to.not.be.jsonSchema(schema);
+    });
+  });
+
+  describe('intent', () => {
+    it('should validate', () => {
+      chai.expect(loadSample('request', 'intent', 'base.json'))
+        .to.be.jsonSchema(schema);
     });
 
-    it('should have always returned hello', () => {
-      expect(alexaSchemas.greet).to.have.always.returned('hello');
+    it('should validate with session attributes', () => {
+      chai.expect(loadSample('request', 'intent', 'withSessionAttributes.json'))
+        .to.be.jsonSchema(schema);
+    });
+
+    it('should not validate without name', () => {
+      chai.expect(loadSample('request', 'intent', 'withoutName.json'))
+        .to.not.be.jsonSchema(schema);
+    });
+  });
+
+  describe('session ended', () => {
+    it('should validate', () => {
+      chai.expect(loadSample('request', 'sessionEnded', 'base.json'))
+        .to.be.jsonSchema(schema);
+    });
+
+    it('should not validate without reason', () => {
+      chai.expect(loadSample('request', 'sessionEnded', 'withoutReason.json'))
+        .to.not.be.jsonSchema(schema);
+    });
+  });
+});
+
+describe('response', () => {
+  let schema;
+
+  before(() => {
+    schema = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'skills-kit',
+      'response.yaml')));
+  });
+
+  describe('speech', () => {
+    it('should validate text', () => {
+      chai.expect(loadSample('response', 'speech', 'text.json'))
+        .to.be.jsonSchema(schema);
+    });
+
+    it('should validate ssml', () => {
+      chai.expect(loadSample('response', 'speech', 'ssml.json'))
+        .to.be.jsonSchema(schema);
+    });
+
+    it('should not validate ssml and text', () => {
+      chai.expect(loadSample('response', 'speech', 'ssmlAndText.json'))
+        .to.not.be.jsonSchema(schema);
+    });
+  });
+
+  describe('card', () => {
+    it('should validate simple with content', () => {
+      chai.expect(loadSample('response', 'card', 'simple.json'))
+        .to.be.jsonSchema(schema);
+    });
+
+    it('should not validate simple with text/image', () => {
+      chai.expect(loadSample('response', 'card', 'simpleWithTextImage.json'))
+        .to.not.be.jsonSchema(schema);
+    });
+
+    it('should validate standard with text/image', () => {
+      chai.expect(loadSample('response', 'card', 'standard.json'))
+        .to.be.jsonSchema(schema);
+    });
+
+    it('should not validate standard with content', () => {
+      chai.expect(loadSample('response', 'card', 'standardWithContent.json'))
+        .to.not.be.jsonSchema(schema);
+    });
+
+    it('should validate link account', () => {
+      chai.expect(loadSample('response', 'card', 'linkAccount.json'))
+        .to.be.jsonSchema(schema);
+    });
+
+    it('should not validate link with extra title/content/text/image', () => {
+      chai.expect(loadSample('response', 'card', 'linkAccountWithExtra.json'))
+        .to.not.be.jsonSchema(schema);
     });
   });
 });
